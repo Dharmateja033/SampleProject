12eca7f274b0b24ea8847cc2dede903186ba0b08
Dharma <dharmateja.e.97@gmail.com>
2022-10-24 11:12:24 +0530

    assignment 1

---
+++ Q2.java
@@ -0,0 +1,16 @@
+package assignment1;
+
+public class Q2 {
+	public static void main(String[] args) {
+		int n=4;
+		
+		for(int i=1;i<=n;i++)
+		{
+			System.out.print("    ");
+			for(int j=1;j<=n;j++)
+				System.out.print(i+" ");
+			System.out.println();
+		}
+
+		}
+}
---
+++ Q4.java
@@ -0,0 +1,25 @@
+package assignment1;
+
+public class Q4 {
+
+	public static void main(String[] args) {
+		int n=23;
+		System.out.println();
+		System.out.println();
+		for(int i=0;i<n;i++)
+		{
+			System.out.print("          ");
+			for(int j=0;j<n;j++)
+			{
+				if(j==0&&i>(n-1)/2||i==n-1||j==n-1&&i>(n-1)/2||
+					i-j>=(n-1)/2||i+j>=(n-1)+(n-1)/2||i==n-2)
+					System.out.print("#");
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
